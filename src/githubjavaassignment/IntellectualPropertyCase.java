08f5201e914bbef2ea830810a00d7b3aa2c22d9a
350265507 <350265507@C-J55TKB2.tdsb.on.ca>
2026-05-14 09:20:46 -0400

    Create the classes by Yonghang

---
+++ src/githubjavaassignment/IntellectualPropertyCase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package githubjavaassignment;
+
+/**
+ *
+ * @author 350265507
+ */
+public class IntellectualPropertyCase {
+    
+}

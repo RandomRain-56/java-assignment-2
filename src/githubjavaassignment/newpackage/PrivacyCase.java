5b349cf2db1a1ea551cda8fdd14c6f53d163ca61
350325485 <350325485@C-5W0XS13.tdsb.on.ca>
2026-05-14 10:03:57 -0400

    Coding the part of UML by yuhong

--- src/githubjavaassignment/newpackage/PrivacyCase.java
+++ src/githubjavaassignment/newpackage/PrivacyCase.java
@@ -8,6 +8,31 @@ package githubjavaassignment.newpackage;
  *
  * @author 350265507
  */
-public class PrivacyCase {
-    
+public class PrivacyCase extends EthicsCase{
+    //add some private variabel
+    private String dataType;
+    /**
+     * constructor
+     * @param caseTitle variable for parent class
+     * @param description variable for parent class
+     * @param dataType variable for parent class
+     */
+    public PrivacyCase(String caseTitle, String description,String dataType){
+        super(caseTitle, description);
+        this.dataType = dataType;
+    }
+    /**
+     * give the data type 
+     * @return give the data type
+     */
+    public String getDataType(){
+        return dataType;
+    }
+    /**
+     * give out the basic information
+     * @return give out the basic information
+     */
+    public String toString(){
+        return dataType;
+    }
 }
--- src/githubjavaassignment/newpackage/Verdict.java
+++ src/githubjavaassignment/newpackage/Verdict.java
@@ -9,5 +9,73 @@ package githubjavaassignment.newpackage;
  * @author 350265507
  */
 public class Verdict {
-    
+    //create private variables
+    private String caseName;
+    private String studentVerdict;
+    private String reason;
+    /**
+     * empty constructor
+    */
+    public Verdict(){
+        caseName = "empty";
+        studentVerdict = " empty";
+        reason = "empty";
+    }
+    /**
+     * constructor with parament 
+     */
+    public void verdict(String caseName, String verdict,String reason){
+        this.caseName = caseName;
+        this.reason = reason;
+        this.studentVerdict = verdict;
+    }
+    /**
+     * set a new value to Case Name
+     * @param caseName new value to the Case name
+     */
+    public void setCaseName(String caseName){
+        this.caseName = caseName;
+    }
+    /**
+     * get the current name 
+     * @return give the value out
+     */
+    public String getCaseName(){
+        return caseName;
+    }
+    /**
+     * set the new value to the reason
+     * @param reason new reason to store
+     */
+    public void setReason(String reason){
+        this.reason = reason;
+    }
+    /**
+     * get the current reason
+     * @return give the reaosn out
+     */
+    public String getReason(){
+        return reason;
+    }
+    /**
+     * set the new student verdict
+     * @param studentVerdict new Verdict need to store
+     */
+    public void setStudentVerdict(String studentVerdict){
+        this.studentVerdict = studentVerdict;
+    }
+    /**
+     * get the current studentVerdict
+     * @return give the studentVerdict
+     */
+    public String getStudentVerdict(){
+        return studentVerdict;
+    }
+    /**
+     * give out the describe for the file
+     * @return give the object info
+     */
+    public String toString(){
+        return caseName+studentVerdict + reason;
+    }
 }

c694112fc24d39bf4e84440f7fcda243fdc92487
349942589 <349942589@C-5VXTS13.tdsb.on.ca>
2026-05-14 10:15:30 -0400

    Add coment fro IP class

--- src/githubjavaassignment/newpackage/IntellectualPropertyCase.java
+++ src/githubjavaassignment/newpackage/IntellectualPropertyCase.java
@@ -9,19 +9,33 @@ package githubjavaassignment.newpackage;
  * @author 350265507
  */
 public class IntellectualPropertyCase extends EthicsCase{
-    
+   
     private String ipType;
-    
+    /**Constructs an IntellectualPropertyCase object with
+     * the specified title, description, and intellectual property type
+     * 
+     * @param caseTitle the title of the ethics case
+     * @param description the description of the ethics case
+     * @param ipType the type of intellectual property involved
+     */
     public IntellectualPropertyCase(String caseTitle, String description, String ipType) {
         super(caseTitle, description, "IntellectualProperty");
         this.ipType = ipType;
         
     }
-    
+    /**
+     * Returns the type of intellectual property involved in the case
+     * 
+     * @return the intellectual property type
+     */
     public String getIpType(){
         return this.ipType;
     }
-    
+    /**
+     * Returns a string representation of the intellectual property case.
+     * 
+     * @return a string containing the case details and IP types
+     */
     public String toString(){
         return super.toString() + this.ipType;
     }
